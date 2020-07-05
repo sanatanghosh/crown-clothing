4719d3c0e42ae07e8fae9e74fb92f30b5f3e79cf
sanatanghosh <sanatanghosh1997@gmail.com>
2020-07-05 22:21:23 +0530

    sign in and sign up for the user with firebase auth and check user's email and password

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
-import { auth } from './firebase/firebase.utils';
+import { auth ,createUserProfileDocument} from './firebase/firebase.utils';
  
 
 
@@ -27,10 +27,23 @@ class App extends React.Component {
 
 componentDidMount() {
 
-  this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-    this.setState({ currentUser: user});
+  this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    if (userAuth) {
+      const userRef = await createUserProfileDocument(userAuth);
 
-    console.log(user);
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        });
+
+        // console.log(this.state);
+      });
+    }
+
+    this.setState({ currentUser: userAuth });
   });
 }
 
