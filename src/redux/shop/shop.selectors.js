df86a481245ec67585180307116fc4d0c92c5b3d
sanatanghosh <sanatanghosh1997@gmail.com>
2020-07-08 18:33:41 +0530

    normalization and collection pages

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,17 @@
 import { createSelector } from 'reselect';
 
+
+
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+);
+
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectCollections],
+        collections => collections[collectionUrlParam]
+        
 );
